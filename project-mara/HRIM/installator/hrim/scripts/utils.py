b00b2c29c2713900a78a8808b9c12ec672d04d87
Chaoyue Fei <chaoyue.fei@vub.be>
2025-04-11 15:33:22 +0200

    remove duplicate ros2 packages

--- project-mara/HRIM/installator/hrim/scripts/utils.py
+++
@@ -1,7 +0,0 @@
-
-# shorthand for tabulation
-def getTabs(num):
-    ret = ""
-    for _ in range(num):
-        ret += "\t"
-    return ret
--- project-mara/launching_scripts/launch_mara.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-
-# Source where the simulation files will be
-MARA_SIM_PATH=/home/simulations/ros2_sims_ws/install/setup.bash
-source $MARA_SIM_PATH
-source /usr/share/gazebo/setup.sh
-ros2 launch mara_gazebo mara.launch.py
